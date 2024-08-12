158f4182c214a991b8b4d083d80e0c719a0f6f10
Mahmoud Elagamy <mahmoudelagamy474@gmail.com>
2024-08-12 06:56:44 +0300

    removed top:110% property and made it 100% in all devices

--- src/components/ui/Tooltip.tsx
+++ src/components/ui/Tooltip.tsx
@@ -27,7 +27,7 @@ const Tooltip = ({ message, isVisible, hideTooltip }: TooltipProps) => {
           animate={{ opacity: 1, y: 0 }}
           exit={{ opacity: 0, y: -10 }}
           transition={{ duration: 0.2 }}
-          className={`absolute top-full md:top-[110%] ${currentStep === 2 && "lg:left-1/3"} z-[1] mt-1 rounded bg-Strawberry-red p-2 text-sm text-white shadow-lg`}
+          className={`absolute top-full ${currentStep === 2 && "lg:left-1/3"} z-[1] rounded bg-Strawberry-red p-2 text-sm text-white shadow-lg`}
         >
           <span className="absolute bottom-full left-1/2 h-0 w-0 -translate-x-1/2 transform border-b-8 border-l-8 border-r-8 border-Strawberry-red border-l-transparent border-r-transparent border-t-transparent"></span>
 
